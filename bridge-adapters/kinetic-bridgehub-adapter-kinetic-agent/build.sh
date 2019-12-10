b606c5a44626bd5c5284e9a25f98be25b3ecca18
Austin Peters <austin.peters@kineticdata.com>
2019-12-10 12:20:18 -0600

    Bug fix for kinetic-agent bridge adapter

--- bridge-adapters/kinetic-bridgehub-adapter-kinetic-agent/build.sh
+++ bridge-adapters/kinetic-bridgehub-adapter-kinetic-agent/build.sh
@@ -4,5 +4,5 @@
 set -e
 
 # Build the jar
-docker build -f Dockerfile.builder -t kineticdata/bridgehub-adapter-bmc-ars-rest-builder:latest .
-docker run -v $PWD:/project -w=/project kineticdata/bridgehub-adapter-bmc-ars-rest-builder:latest
+docker build -f Dockerfile.builder -t kineticdata/bridgehub-adapter-kinetic-agent-builder:latest .
+docker run -v $PWD:/project -w=/project kineticdata/bridgehub-adapter-kinetic-agent-builder:latest
